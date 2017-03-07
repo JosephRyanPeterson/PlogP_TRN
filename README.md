a9adaafb9d97960c7cd1153b702f547debae7b39
Joseph Peterson <josuf.the.uf@gmail.com>
2017-03-06 23:32:12 -0600

    Working on a first implementation.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # PlogP_TRN
 Reconstruct a Transcriptional Regulatory Network using the principle of Maximum Entropy.
+
+## About
+
+PlogP_TRN implements a regulatory network reconstruction method based on a method proposed by [T.R. Lezon et al. Using the principle of entropy maximization to infer genetic interaction networks from gene expression patterns, PNAS, 2006, 103(50):19033-19038 doi:10.1073/pnas.0609152103](http://doi.org/10.1073/pnas.0609152103).
+
+Briefly, the method is based on finding <a href="https://www.codecogs.com/eqnedit.php?latex=\rho&space;(\vec{x})" target="_blank"><img src="https://latex.codecogs.com/gif.latex?\rho&space;(\vec{x})" title="\rho (\vec{x})" /></a> where <a href="https://www.codecogs.com/eqnedit.php?latex=\vec{x}" target="_blank"><img src="https://latex.codecogs.com/gif.latex?\vec{x}" title="\vec{x}" /></a> are the observed gene expression values by maximizing the Shannon entropy:
+
+<a href="https://www.codecogs.com/eqnedit.php?latex=S=-\sum_{\vec{x}}\rho&space;(\vec{x})ln\rho&space;(\vec{x})" target="_blank"><img src="https://latex.codecogs.com/gif.latex?S=-\sum_{\vec{x}}\rho&space;(\vec{x})ln\rho&space;(\vec{x})" title="S=-\sum_{\vec{x}}\rho (\vec{x})ln\rho (\vec{x})" /></a> 
+
+which leads to a Boltzmann(-ish) distribution:
+
+<a href="https://www.codecogs.com/eqnedit.php?latex=\rho&space;(\vec{x})\propto&space;e^{-\frac{\sum_{ij}x_{i}M_{ij}x_{j}}{2}}" target="_blank"><img src="https://latex.codecogs.com/gif.latex?\rho&space;(\vec{x})\propto&space;e^{-\frac{\sum_{ij}x_{i}M_{ij}x_{j}}{2}}" title="\rho (\vec{x})\propto e^{-\frac{\sum_{ij}x_{i}M_{ij}x_{j}}{2}}" /></a>
+
+where <a href="https://www.codecogs.com/eqnedit.php?latex=M_{ij}" target="_blank"><img src="https://latex.codecogs.com/gif.latex?M_{ij}" title="M_{ij}" /></a> is the "interaction strength" between genes *i* and *j*.
+
+## Installation
+
+Download the 
+
+Requirements:
+
+* [Julia](http://julialang.org)
+* [IJulia](https://github.com/JuliaLang/IJulia.jl)
+
+## Usage
+
+
---
+++ scripts/runPlogP.jl
@@ -0,0 +1,91 @@
+#=
+MIT License
+
+Copyright (c) 2017 Joseph R. Peterson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+=#
+
+# External Libraries
+using DataTables
+using ArgParse
+
+
+# Create command line parser and parse arguments
+function parse_cmd_line()
+	argSettings = ArgParseSettings()
+	@add_arg_table argSettings begin
+		"--expressionData", "-i"
+			help = "A tab/comma separated file containing gene expression data."
+			arg_type = String
+			required = true
+		"--outputDir", "-o"
+			help = "Name of directory into which results are stored."
+			arg_type = String
+			default = "out"
+		"--significant", "-s"
+			help = "Significant interactions are those 's' standard deviations away from mean."
+			arg_type = Float64
+			default = 2.0
+		"--plot", "-p"
+			help = "Plot intermediate results"
+			default = false
+			action = :store_true
+	end
+
+	# Actually pargse aguments
+	return parse_args(argSettings)
+end
+
+
+
+
+
+# Main computes the network 
+function main()
+	# Get command line arguments 
+	args = parse_cmd_line()
+
+	# Read data files
+	println("Reading inputs...")
+	expressionData = readtable(args["expressionData"])
+
+	# Run algorithm
+	println("Computing couplings...")
+	
+
+	# Cull network
+	println("Thresholding network...")
+	
+
+	# Plot Data (if requested)
+	if args["plot"]
+		println("Plotting...")
+	end
+
+	# Save resulting weight and graph files
+	println("Saving results...")
+
+
+	# Successfully completed
+	println("Done!")
+end
+main()
+
+
